5beb0cbf248bfbf91be9eda752cc3348d7952ef7
mostafamasrya <106432717+mostafamasrya@users.noreply.github.com>
2022-07-18 15:47:20 +0200

    delete vars

--- FinalProject2022/finalproj/finalproj/settings.py
+++ FinalProject2022/finalproj/finalproj/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os.path
 import os
 from pathlib import Path
-from .vars import *
+# from .vars import *
+from vars import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
