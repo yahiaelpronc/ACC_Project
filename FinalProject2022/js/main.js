4ff85eed60cdd72a1a3de403174d4982b5777e00
ahmed2729 <59785686+ahmed2729@users.noreply.github.com>
2022-07-18 23:26:47 +0200

    project

--- FinalProject2022/js/main.js
+++ FinalProject2022/js/main.js
@@ -1,77 +1,20 @@
-/*
- ** Countdown Timer
- ** Video URL: https://www.youtube.com/watch?v=eFsiOTJrrE8
- */
-
-// The End Of The Year Date
-// 1000 milliseconds = 1 Second
-
-let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
-// console.log(countDownDate);
-
-let counter = setInterval(() => {
-  // Get Date Now
-  let dateNow = new Date().getTime();
-
-  // Find The Date Difference Between Now And Countdown Date
-  let dateDiff = countDownDate - dateNow;
-
-  // Get Time Units
-  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
-  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
-
-  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
-  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
-  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
-
-  if (dateDiff < 0) {
-    clearInterval(counter);
+/* Whatsapp Chat Widget by www.idblanter.com */
+$(document).on("click", "#send-it", function () {
+  var a = document.getElementById("chat-input");
+  if ("" != a.value) {
+    var b = $("#get-number").text(),
+      c = document.getElementById("chat-input").value,
+      d = "https://web.whatsapp.com/send",
+      e = b,
+      f = "&text=" + c;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) var d = "whatsapp://send";
+    var g = d + "?phone=" + e + f;
+    window.open(g, '_blank')
   }
-}, 1000);
-
-/*
- ** Animate Width On Scrolling
- ** Video URL: https://youtu.be/sbIoIKI9FOc
- */
-
-/*
- ** Increase Numbers On Scrolling
- ** Video URL: https://youtu.be/PLsUdgLnzgQ
- */
-
-let progressSpans = document.querySelectorAll(".the-progress span");
-let section = document.querySelector(".our-skills");
-
-let nums = document.querySelectorAll(".stats .number");
-let statsSection = document.querySelector(".stats");
-let started = false; // Function Started ? No
-
-window.onscroll = function () {
-  // Skills Animate Width
-  if (window.scrollY >= section.offsetTop - 250) {
-    progressSpans.forEach((span) => {
-      span.style.width = span.dataset.width;
-    });
-  }
-  // Stats Increase Number
-  if (window.scrollY >= statsSection.offsetTop) {
-    if (!started) {
-      nums.forEach((num) => startCount(num));
-    }
-    started = true;
-  }
-};
-
-function startCount(el) {
-  let goal = el.dataset.goal;
-  let count = setInterval(() => {
-    el.textContent++;
-    if (el.textContent == goal) {
-      clearInterval(count);
-    }
-  }, 2000 / goal);
-}
+}), $(document).on("click", ".informasi", function () {
+  document.getElementById("get-number").innerHTML = $(this).children(".my-number").text(), $(".start-chat,.get-new").addClass("show").removeClass("hide"), $(".home-chat,.head-home").addClass("hide").removeClass("show"), document.getElementById("get-nama").innerHTML = $(this).children(".info-chat").children(".chat-nama").text(), document.getElementById("get-label").innerHTML = $(this).children(".info-chat").children(".chat-label").text()
+}), $(document).on("click", ".close-chat", function () {
+  $("#whatsapp-chat").addClass("hide").removeClass("show")
+}), $(document).on("click", ".blantershow-chat", function () {
+  $("#whatsapp-chat").addClass("show").removeClass("hide")
+});
